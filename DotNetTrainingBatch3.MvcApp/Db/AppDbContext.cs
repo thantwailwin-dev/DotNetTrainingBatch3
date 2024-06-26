3a2169de15a1fc0c6dc5e37b08c23b52e4e393f8
ThantWaiLwin <152428057+ThantWL@users.noreply.github.com>
2024-06-27 00:41:03 +0630

    ASP.NET Core MVC with ApexChart

--- DotNetTrainingBatch3.MvcApp/Db/AppDbContext.cs
+++ DotNetTrainingBatch3.MvcApp/Db/AppDbContext.cs
@@ -18,5 +18,7 @@ namespace DotNetTrainingBatch3.MvcApp.Db
         }
 
         public DbSet<BlogModel> Blogs { get; set; }
+        public DbSet<PageStatisticsModel> PageStatistics { get; set; }
+        public DbSet<RadarModel> Radars { get; set; }
     }
 }
--- DotNetTrainingBatch3.MvcApp/Models/BlogModel.cs
+++ DotNetTrainingBatch3.MvcApp/Models/BlogModel.cs
@@ -24,4 +24,33 @@ namespace DotNetTrainingBatch3.MvcApp.Models
         public bool IsSuccess { get; set; }
         public string Message { get; set; }
     }
+
+    [Table("Tbl_PageStatistics")]
+    public class PageStatisticsModel
+    {
+        [Key]
+        public int PageStatistics { get; set; }
+        public int SessionDuration { get; set; }
+        public int PageViews { get; set; }
+        public int TotalVisits { get; set; }
+        public string CreatedDate { get; set; }
+
+    }
+
+    [Table("Tbl_RadarChart")]
+    public class RadarModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Month { get; set; }
+        public int Series { get; set; }
+    }
+
+    public class ApexChartRadarResponseModel
+    {      
+        public List<int> Series { get; set; }
+        public List<string> Lables { get; set; }
+    }
+
+
 }
