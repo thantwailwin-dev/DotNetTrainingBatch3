154e18849f887f36ae5938be030d12d959778f75
ThantWaiLwin <152428057+ThantWL@users.noreply.github.com>
2024-07-05 14:07:10 +0630

    ASP.NET Core MVC Pagination

---
+++ DotNetTrainingBatch3.MvcApp/Controllers/BlogPaginationController.cs
@@ -0,0 +1,40 @@
+﻿using DotNetTrainingBatch3.MvcApp.Db;
+using DotNetTrainingBatch3.MvcApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetTrainingBatch3.MvcApp.Controllers
+{
+	public class BlogPaginationController : Controller
+    {
+        [ActionName("Index")]
+        public IActionResult BlogIndex(int pageNo = 1,int pageSize = 10)
+		{
+            AppDbContext _appDbContext = new AppDbContext();
+            int rowCount = _appDbContext.Blogs.Count();
+            int pageCount = rowCount / pageSize;
+
+            if (rowCount % pageSize > 0)
+                pageCount++;
+
+            if (pageNo > pageCount)
+            {
+                return Redirect("/EFCoreBlog");
+            }
+
+
+            List<BlogModel> lst = _appDbContext.Blogs
+                /*.OrderByDescending(x => x.BlogId)*/
+                .Skip((pageNo - 1) * pageSize)
+                .Take(pageSize)
+            .ToList();
+
+
+            BlogResponseModel model = new BlogResponseModel();
+            model.Data = lst;
+            model.PageNo = pageNo;
+            model.PageSize = pageSize;
+            model.PageCount = pageCount;
+            return View("BlogIndex",model);
+		}
+	}
+}
