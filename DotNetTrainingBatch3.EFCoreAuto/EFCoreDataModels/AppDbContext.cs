5f382d9800aaf8a2c259aade6d439ad57febe359
ThantWaiLwin <152428057+ThantWL@users.noreply.github.com>
2024-07-12 14:33:05 +0630

    Blazor Server CRUD

--- DotNetTrainingBatch3.EFCoreAuto/EFCoreDataModels/AppDbContext.cs
+++ DotNetTrainingBatch3.EFCoreAuto/EFCoreDataModels/AppDbContext.cs
@@ -16,11 +16,7 @@ public partial class AppDbContext : DbContext
     }
 
     public virtual DbSet<TblBlog> TblBlogs { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=AceInternship;User ID=sa;Password=twl@123;TrustServerCertificate=True");
-
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<TblBlog>(entity =>
