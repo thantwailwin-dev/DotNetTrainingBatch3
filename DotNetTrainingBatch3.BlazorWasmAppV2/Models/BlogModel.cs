2830cfedf6189e891d04c1e85a5d2ce047c5276f
ThantWaiLwin <152428057+ThantWL@users.noreply.github.com>
2024-07-15 10:05:26 +0630

    Blazor Web Assembly Blog CRUD

---
+++ DotNetTrainingBatch3.BlazorWasmAppV2/Models/BlogModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetTrainingBatch3.BlazorWasmAppV2.Models
+{
+    [Table("Tbl_Blog")]
+    public class BlogModel
+    {
+        [Key]
+        public string BlogId { get; set; }
+        public string? BlogTitle { get; set; }
+        public string? BlogAuthor { get; set; }
+        public string? BlogContent { get; set; }
+    }
+}
